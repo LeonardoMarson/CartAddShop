1c5921ec3c7d6225263dc00d6042720fddb28ff9
LeonardoMarson <leonardomateusmarson@gmail.com>
2023-05-25 10:51:20 -0300

    Modificações a fins de privacitade dos desenvolvedores
    Foi removido conexão com a base de dados pessoal de um dos desenvolvedores e deixado apenas o localhost, além de ter deixado apenas uma nota como exemplo a fins de deixar menos arquivos junto ao código

--- src/bd/Connect.java
+++ src/bd/Connect.java
@@ -10,10 +10,6 @@ public class Connect {
 	private static String conect2 = "root";
 	private static String conect3 = "";
 	
-//	public static String conect1 = "jdbc:mysql://br470.hostgator.com.br:3306/farof014_teste";
-//	public static String conect2 = "farof014_aulapoo";
-//	public static String conect3 = "G7d5g3b8%";
-
 	
 	private static Connection instance = null;
 	
